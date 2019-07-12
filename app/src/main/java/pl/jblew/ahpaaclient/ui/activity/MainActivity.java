9afd0d1b4a8284d4bb20278150becb307e593192
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-12 11:11:49 +0200

    Fixed #33 — add icons

--- app/src/main/java/pl/jblew/ahpaaclient/ui/activity/MainActivity.java
+++ app/src/main/java/pl/jblew/ahpaaclient/ui/activity/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     AndroidInjection.inject(this);
+    setTheme(R.style.AppTheme);
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     Toolbar toolbar = findViewById(R.id.toolbar);

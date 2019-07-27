9209f2b3199d31018f5c26edc78e205e23ad09ce
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-27 11:50:13 +0200

    feat: log only in debug mode

--- app/src/main/java/pl/jblew/ahpaaclient/AppController.java
+++ app/src/main/java/pl/jblew/ahpaaclient/AppController.java
@@ -24,10 +24,20 @@ package pl.jblew.ahpaaclient;
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 import pl.jblew.ahpaaclient.di.component.DaggerAppComponent;
+import timber.log.Timber;
 
 public class AppController extends DaggerApplication {
   @Override
   protected AndroidInjector<? extends AppController> applicationInjector() {
     return DaggerAppComponent.builder().create(this);
+  }
+  
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree());
+    }
   }
 }
